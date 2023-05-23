c2a8a010802478dc0600f7b2f32a2be475567803
Shaashwat05 <shaas2000@gmail.com>
2023-05-23 15:30:21 -0500

    single hand

--- hand_detection.py
+++ hand_detection.py
@@ -2,7 +2,7 @@ from cvzone.HandTrackingModule import HandDetector
 import cv2
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8, maxHands=2)
+detector = HandDetector(detectionCon=0.8, maxHands=1)
 while True:
     # Get image frame
     success, img = cap.read()
@@ -19,20 +19,8 @@ while True:
         handType1 = hand1["type"]  # Handtype Left or Right
 
         fingers1 = detector.fingersUp(hand1)
-
-        if len(hands) == 2:
-            # Hand 2
-            hand2 = hands[1]
-            lmList2 = hand2["lmList"]  # List of 21 Landmark points
-            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
-            centerPoint2 = hand2['center']  # center of the hand cx,cy
-            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
-
-            fingers2 = detector.fingersUp(hand2)
-
-            # Find Distance between two Landmarks. Could be same hand or different hands
-            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
-            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
+        print(fingers1.count(1))
+        
     # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
