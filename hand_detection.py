21d27f0ce83a791d95873bc18688d2a35e44cb92
Shaashwat05 <shaas2000@gmail.com>
2023-05-27 11:13:09 -0500

    gestures

--- hand_detection.py
+++ hand_detection.py
@@ -1,5 +1,16 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+
+model = load_model('mp_hand_gesture')
+
+# Load class names
+f = open('gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=1)
@@ -19,6 +30,12 @@ while True:
         centerPoint1 = hand1['center']  # center of the hand cx,cy
         handType1 = hand1["type"]  # Handtype Left or Right
 
+        prediction = model.predict([np.array(lmList1)[:, :2].tolist()], verbose=0)
+        classID = np.argmax(prediction)
+        className = classNames[classID]
+        #img = cv2.putText(img, className, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+   
+
         fingers1 = detector.fingersUp(hand1)
         #print(fingers1.count(1))
         if fingers1.count(1) == 1:
@@ -27,6 +44,10 @@ while True:
         if fingers1.count(1) == 5:
             # mode = erase
             coors = (lmList1[12][0], lmList1[12][1])
+        
+        if fingers1.count(1) == 0 or className == 'Fist':
+            # model = erase everything 
+            pass
         
 
     # Display
